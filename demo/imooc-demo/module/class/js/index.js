ac9dcddb51688ae84e473606e8af40069ab6658a
头头萍 <876772499@qq.com>
2016-11-24 16:34:26 +0800

    Update

--- demo/imooc-demo/module/class/js/index.js
+++ demo/imooc-demo/module/class/js/index.js
@@ -2,12 +2,15 @@ window.onscroll = function(){
     var t = document.documentElement.scrollTop || document.body.scrollTop; 
     var nav = document.getElementById( "section-nav-class" );
     var fixnav = document.getElementById( "section-fixnav-class" );
+    var backTop = document.getElementById( "backTop" );
     var top = document.getElementById("position-top").offsetTop + 12;
     if( t >= top) {
         nav.style.display = "none";
         fixnav.style.display = "block";
+        backTop.style.display = "block";
     } else {
         nav.style.display = "block";
         fixnav.style.display = "none";
+        backTop.style.display = "none";
     }
 }
